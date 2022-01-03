07f771aaaec70b1823d552e96d5d9322f0937456
James Johnson <johnsojr@umn.edu>
2022-01-03 16:29:32 -0600

    add handling for middleware

--- src/common/api.service.js
+++ src/common/api.service.js
@@ -1,17 +1,37 @@
+import pipe from "../utils/pipe";
 import mockTour from "./__mocks__/mockTour.json";
 import mockListOfAllTours from "./__mocks__/mockListOfAllTours.json";
 
-const mockResponse = (payload, ms = 300) =>
-  new Promise((resolve) => setTimeout(() => resolve(payload), ms));
+const mockResponse = (payload, middlewares) =>
+  new Promise((resolve) =>
+    setTimeout(() => {
+      pipe([...middlewares, resolve])(payload);
+    }, 300)
+  );
 
+const middlewares = [];
 export const toursService = {
   get(tourId) {
-    return mockResponse({
-      ...mockTour,
-      id: tourId,
-    });
+    return mockResponse(
+      {
+        ...mockTour,
+        id: tourId,
+      },
+      middlewares
+    );
   },
   list() {
     return mockResponse(mockListOfAllTours);
   },
+  use(middleware) {
+    middlewares.push(middleware);
+  },
 };
+
+// change any api locale keys from "English" to "en"
+const localeEnglishToEn = (json) => {
+  const newJSON = JSON.stringify(json).replace('"English":', '"en":');
+  return JSON.parse(newJSON);
+};
+
+toursService.use(localeEnglishToEn);
