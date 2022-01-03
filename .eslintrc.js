98cfe4632df7204dc0fa74e013d1278177255c03
James Johnson <johnsojr@umn.edu>
2022-01-03 17:20:18 -0600

    prefer non-hyphenated attributes

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,6 @@ module.exports = {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error',
     "vue/require-default-prop": "off",
+    "vue/attribute-hyphenation": "never",
   },
 };
