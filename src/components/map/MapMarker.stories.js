889baceec98e95ca1bcd1f0e604c0e272866172f
James Johnson <johnsojr@umn.edu>
2022-01-08 11:47:40 -0600

    add color prop

---
+++ src/components/map/MapMarker.stories.js
@@ -0,0 +1,44 @@
+import Map from "./Map.vue";
+import MapMarker from "./MapMarker.vue";
+import mockTour from "../../common/__mocks__/mockTour.json";
+import getStagesFromTourWhere from "../../utils/getStagesFromTourWhere.js";
+
+export default {
+  title: "Camino/Map/MapMarker",
+  component: MapMarker,
+};
+
+const Template = (args) => ({
+  components: { Map, MapMarker },
+  setup() {
+    return { args };
+  },
+  template: `
+    <Map v-bind="args.map">
+      <MapMarker v-bind="args.startMarker" />
+      <MapMarker v-bind="args.targetMarker" />
+    </Map>
+  `,
+});
+
+const navStages = getStagesFromTourWhere("type", "navigation", mockTour);
+const navStagesWithRoutes = navStages.filter((stage) => stage?.route.length);
+const stage1 = navStagesWithRoutes[1];
+
+export const Default = Template.bind({});
+Default.args = {
+  map: {
+    lng: mockTour.start_location.lng,
+    lat: mockTour.start_location.lat,
+    zoom: 15,
+  },
+  startMarker: {
+    lng: mockTour.start_location.lng,
+    lat: mockTour.start_location.lat,
+  },
+  targetMarker: {
+    lng: stage1.targetPoint.lng,
+    lat: stage1.targetPoint.lat,
+    color: "red",
+  },
+};
--- src/components/map/MapMarker.vue
+++ src/components/map/MapMarker.vue
@@ -8,22 +8,22 @@ import { Marker } from "mapbox-gl";
 const props = defineProps({
   lng: number().isRequired,
   lat: number().isRequired,
-  // unique ID for this data source
-  // id: string().isRequired,
+  color: string(),
 });
 
 const mapRef = inject("mapRef", null);
 
-// watch mapRef. When there's a valid Map
-// listen for the load event and then
-// add the polyline
 watch(
   () => mapRef.value,
   () => {
     const map = mapRef.value;
     if (!map) return;
     map.on("load", () => {
-      new Marker().setLngLat([props.lng, props.lat]).addTo(map);
+      new Marker({
+        color: props.color,
+      })
+        .setLngLat([props.lng, props.lat])
+        .addTo(map);
     });
   }
 );
