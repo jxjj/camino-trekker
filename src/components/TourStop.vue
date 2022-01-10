02b149ee8a9cfc6c21a4684b64426a776a44839d
James Johnson <johnsojr@umn.edu>
2022-01-09 20:56:31 -0600

    bottom navs for now

--- src/components/TourStop.vue
+++ src/components/TourStop.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tour-stop">
     <TourHeader
-      v-if="stopIndex === 0"
+      v-if="isFirstStop"
       class="container"
       :tour="tour"
       :stopIndex="stopIndex"
@@ -12,18 +12,37 @@
       <section v-for="stage in stages" :key="stage.id">
         <Stage :stage="stage" :locale="locale" />
       </section>
-      <footer class="tour-page__stop-footer">
-        <Button
-          v-if="stopIndex !== tour?.stops?.length - 1"
-          icon="arrow_forward"
-          iconPosition="end"
-          variant="primary"
-          @click="$router.push(nextStopUrl)"
-        >
-          Continue
-        </Button>
-      </footer>
+      <Button
+        v-if="stopIndex !== tour?.stops?.length - 1"
+        icon="arrow_forward"
+        iconPosition="end"
+        variant="primary"
+        @click="$router.push(nextStopUrl)"
+      >
+        Continue
+      </Button>
     </div>
+    <router-link
+      v-if="!isFirstStop"
+      :to="`/tours/${tour.id}/stops/${stopIndex - 1}`"
+    >
+      <FAB
+        v-if="!isFirstStop"
+        class="tour-stop__prev-button"
+        icon="arrow_back"
+        alt="Previous Stop"
+      />
+    </router-link>
+    <router-link
+      v-if="!isLastStop"
+      :to="`/tours/${tour.id}/stops/${stopIndex + 1}`"
+    >
+      <FAB
+        class="tour-stop__next-button"
+        icon="arrow_forward"
+        alt="Next Stop"
+      />
+    </router-link>
   </div>
 </template>
 <script setup>
@@ -33,18 +52,34 @@ import TourHeader from "./TourHeader.vue";
 import Stage from "./Stage.vue";
 import { computed } from "vue";
 import { useStore } from "vuex";
+import FAB from "./FAB.vue";
 
 const store = useStore();
 const stopIndex = computed(() => store.getters.stopIndex);
 const tour = computed(() => store.state.tour);
 const stop = computed(() => store.getters.currentStop);
+const isFirstStop = computed(() => store.getters.isFirstStop);
+const isLastStop = computed(() => store.getters.isLastStop);
 const stages = computed(() => stop.value?.stop_content?.stages) || [];
 const locale = computed(() => store.state.locale);
 </script>
-<style scope>
+<style scoped>
+.tour-stop {
+  position: relative;
+}
 .tour-stop__stages {
   background: var(--white);
   border-radius: 2rem 2rem 0 0;
   padding-bottom: 12rem;
+}
+.tour-stop__prev-button {
+  position: fixed;
+  bottom: 5rem;
+  left: 2rem;
+}
+.tour-stop__next-button {
+  position: fixed;
+  bottom: 5rem;
+  right: 2rem;
 }
 </style>
