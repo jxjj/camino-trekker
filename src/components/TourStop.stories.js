561855187c3cd11ccec90752cdcdd2f8560a9d09
James Johnson <johnsojr@umn.edu>
2022-01-10 21:09:36 -0600

    mock store in tourstop

--- src/components/TourStop.stories.js
+++ src/components/TourStop.stories.js
@@ -1,5 +1,24 @@
+import { app } from "@storybook/vue3";
+import { createStore } from "vuex";
+import { getters } from "../store/index.js";
 import TourStop from "./TourStop.vue";
 import mockTour from "../common/__mocks__/mockTour.json";
+
+const store = createStore({
+  state: () => ({
+    tour: mockTour,
+    locale: "en",
+    route: {
+      params: {
+        tourId: 6,
+        stopIndex: 1,
+      },
+    },
+  }),
+  getters,
+});
+
+app.use(store);
 
 export default {
   title: "Camino/TourStop",
@@ -17,7 +36,4 @@ const Template = (args) => ({
 });
 
 export const Default = Template.bind({});
-Default.args = {
-  stopIndex: 2,
-  tour: mockTour,
-};
+Default.args = {};
