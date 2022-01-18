b09393a54f425a8aefe37a62cc1c020ef656b7e8
James Johnson <johnsojr@umn.edu>
2022-01-17 20:00:46 -0600

    working test

--- src/components/stages/ARTest/ARTest.vue
+++ src/components/stages/ARTest/ARTest.vue
@@ -8,11 +8,34 @@
         renderer="antialias: true; alpha: true"
       >
         <a-camera gps-projected-camera rotation-reader></a-camera>
-        <a-box
-          gps-projected-entity-place="latitude: 44.810982; longitude: -93.627016"
-          material="color: red"
-          scale="10 10 10"
-        ></a-box>
+        <a-entity
+          gps-projected-entity-place="latitude: 44.810892; longitude: -93.627038"
+        >
+          <a-text
+            value="Hello, world!"
+            color="red"
+            align="center"
+            scale="2 2 2"
+            position="0 1.6 0"
+            look-at="[gps-projected-camera]"
+          ></a-text>
+          <a-plane
+            color="white"
+            position="0 1.6 -0.01"
+            width="3"
+            material="transparent: true; opacity: 0.90"
+            look-at="[gps-projected-camera]"
+          ></a-plane>
+
+          <a-cone
+            radius-bottom="1"
+            height="2"
+            material="color: magenta;"
+            rotation="0 0 180"
+            position="0 -0.25 0"
+          >
+          </a-cone>
+        </a-entity>
       </a-scene>
     </div>
   </div>
@@ -22,6 +45,7 @@
 import Button from "../../Button.vue";
 import { useGeolocation } from "@vueuse/core";
 import "@ar-js-org/ar.js";
+import "aframe-look-at-component";
 import "aframe";
 
 const { coords } = useGeolocation();
