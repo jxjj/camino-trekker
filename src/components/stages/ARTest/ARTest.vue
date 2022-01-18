c82ec2183807652f535c3e0869fe990e01dbce21
James Johnson <johnsojr@umn.edu>
2022-01-17 21:43:28 -0600

    show 4 cardinal directions in test

--- src/components/stages/ARTest/ARTest.vue
+++ src/components/stages/ARTest/ARTest.vue
@@ -3,28 +3,30 @@
     <Button>Show AR</Button>
     <div class="ar-stage__scene-container">
       <a-scene
+        v-if="isLocationLoaded"
         vr-mode-ui="enabled: false"
-        arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false;"
+        arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: true;"
         renderer="antialias: true; alpha: true"
       >
         <a-camera gps-projected-camera rotation-reader></a-camera>
         <a-entity
-          gps-projected-entity-place="latitude: 44.810892; longitude: -93.627038"
+          v-for="(waypoint, i) in waypoints"
+          :key="i"
+          :gps-projected-entity-place="`latitude: ${waypoint.location.lat}; longitude: ${waypoint.location.lng}`"
+          look-at="[gps-projected-camera]"
         >
           <a-text
-            value="Hello, world!"
-            color="red"
+            :value="waypoint.text"
+            color="black"
             align="center"
             scale="2 2 2"
             position="0 1.6 0"
-            look-at="[gps-projected-camera]"
           ></a-text>
           <a-plane
             color="white"
             position="0 1.6 -0.01"
             width="3"
             material="transparent: true; opacity: 0.90"
-            look-at="[gps-projected-camera]"
           ></a-plane>
 
           <a-cone
@@ -42,12 +44,52 @@
 </template>
 
 <script setup>
-import Button from "../../Button.vue";
+import { computed, watch, ref } from "vue";
 import { useGeolocation } from "@vueuse/core";
+import Button from "../../Button.vue";
 import "@ar-js-org/ar.js";
 import "aframe-look-at-component";
 import "aframe";
 
 const { coords } = useGeolocation();
-console.log({ coords });
+const isLocationLoaded = computed(() => !!coords.value.latitude);
+
+const waypoints = ref([]);
+
+watch(
+  () => coords.value,
+  () => {
+    const { longitude, latitude } = coords.value;
+    waypoints.value = [
+      {
+        text: "North",
+        location: {
+          lng: longitude,
+          lat: latitude + 0.0001,
+        },
+      },
+      {
+        text: "East",
+        location: {
+          lng: longitude + 0.0001,
+          lat: latitude,
+        },
+      },
+      {
+        text: "South",
+        location: {
+          lng: longitude,
+          lat: latitude - 0.0001,
+        },
+      },
+      {
+        text: "West",
+        location: {
+          lng: longitude - 0.0001,
+          lat: latitude,
+        },
+      },
+    ];
+  }
+);
 </script>
