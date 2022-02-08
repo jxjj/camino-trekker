a537ac8169be735e6d6c49dcdf159dd63bff0fef
James Johnson <johnsojr@umn.edu>
2022-02-07 21:44:46 -0600

    remove top border

--- src/components/Stage/stages/Separator.vue
+++ src/components/Stage/stages/Separator.vue
@@ -18,7 +18,6 @@ const text = computed(() => props.stage.text[props.locale]);
 </script>
 <style scoped>
 .separator-stage {
-  border-top: 1px solid var(--gray-light);
   padding-top: 1rem;
   margin-top: 1rem;
   margin-bottom: 1rem;
