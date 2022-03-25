129ddb18be55bf8625ac3f20cebc4fce092efbcc
James Johnson <johnsojr@umn.edu>
2022-03-25 14:20:32 -0500

    reduce shadow to match header

--- src/components/BottomNav/BottomNav.vue
+++ src/components/BottomNav/BottomNav.vue
@@ -70,7 +70,7 @@ const isActiveSheet = (sheetKey) => {
   /* TODO: make this happen with autoprefixer */
   -webkit-backdrop-filter: blur(0.5rem);
   backdrop-filter: blur(0.5rem);
-  box-shadow: 0 -1px 3px 0 rgb(0 0 0 / 0.1), 0 -1px 2px -1px rgb(0 0 0 / 0.1);
+  box-shadow: 0 -1px 2px 0 rgb(0 0 0 / 0.1);
 
   display: grid;
   grid-template-columns: min-content minmax(0, 1fr) min-content;
