4372e36f10e710785b189295a20f8fd482ab9dc4
James Johnson <johnsojr@umn.edu>
2022-03-28 16:41:17 -0500

    remove unused

--- src/components/MapPolyline/toGeoJson.js
+++ src/components/MapPolyline/toGeoJson.js
@@ -37,22 +37,3 @@ export const toGeoJsonPoint = (lngLat) => ({
     coordinates: lngLatToArray(lngLat),
   },
 });
-
-// export const tourRoutesToGeoJson = (tour) => {
-//   const navStages = getStagesFromTourWhere("type", "navigation", tour);
-//   const allRoutes = navStages.map((stage) => stage.route);
-//   return {
-//     type: "FeatureCollection",
-//     features: allRoutes.map(toGeoJsonLineString),
-//   };
-// };
-
-// export const tourStopsToGeoJson = (tour) => {
-//   const navStages = getStagesFromTourWhere("type", "navigation", tour);
-//   const allTargetPoints = navStages.map((stage) => stage.targetPoint);
-//   const startPoint = tour.start_location;
-//   return {
-//     type: "FeatureCollection",
-//     features: [startPoint, ...allTargetPoints].map(toGeoJsonPoint),
-//   };
-// };
