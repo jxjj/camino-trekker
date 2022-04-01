7146aa428e2f6e3a71717cfc8115e9f36f8fecbb
James Johnson <johnsojr@umn.edu>
2022-04-01 10:20:53 -0500

    Update Input form

--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="input-group">
-    <label class="input-group__label sr-only">{{ label }}</label>
+    <label
+      class="input-group__label"
+      :class="{
+        'sr-only': labelHidden,
+      }"
+      >{{ label }}</label
+    >
     <input class="input-group__input" v-bind="$attrs" />
   </div>
 </template>
@@ -9,6 +15,10 @@ defineProps({
   label: {
     type: String,
     required: true,
+  },
+  labelHidden: {
+    type: Boolean,
+    default: false,
   },
 });
 </script>
@@ -19,15 +29,18 @@ defineProps({
 }
 .input-group__label {
   display: block;
-  font-size: 0.75rem;
-  color: #333;
-  font-weight: bold;
+  font-size: 0.875rem;
+  color: #404040;
+  font-weight: 500;
 }
 .input-group__input {
+  background: var(--gray-lighter);
   padding: 1rem;
-  border: 2px solid var(--black);
+  /* border: 1px solid var(--gray); */
+  border: none;
   display: block;
   width: 100%;
   border-radius: 0.25rem;
+  filter: drop-shadow(0 1px 1px rgb(0 0 0 / 0.05));
 }
 </style>
