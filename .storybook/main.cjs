8eaa34a7b69a823b396258f8e838386389020e90
James Johnson <johnsojr@umn.edu>
2022-04-04 17:13:36 -0500

    update to @storybook/builder-vite
    storybook-build-vite has been renamed and is now part of the @storybook org

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -4,22 +4,24 @@ module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-essentials"],
   framework: "@storybook/vue3",
-  core: { builder: "storybook-builder-vite" },
-  async viteFinal(config, { configType }) {
+  core: {
+    builder: "@storybook/builder-vite"
+  },
+
+  async viteFinal(config, {
+    configType
+  }) {
     const customVueConfig = {
       template: {
         compilerOptions: {
-          isCustomElement: (tag) => tag.includes("-"),
-        },
-      },
-    };
+          isCustomElement: tag => tag.includes("-")
+        }
+      }
+    }; // replace default vue plugin using one using custom config
 
-    // replace default vue plugin using one using custom config
-    config.plugins = config.plugins.map((plugin) =>
-      plugin.name === "vite:vue" ? vue(customVueConfig) : plugin
-    );
+    config.plugins = config.plugins.map(plugin => plugin.name === "vite:vue" ? vue(customVueConfig) : plugin); // return the customized config
 
-    // return the customized config
     return config;
-  },
+  }
+
 };
